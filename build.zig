9cb979afeeb372e297fb75f2f463a0feeeff7129
CosmicBagel <>
2024-05-15 19:17:44 -0600

    Inital commit

---
+++ build.zig
@@ -0,0 +1,183 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "dungeonhustle",
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    //
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    // b.installArtifact(lib);
+
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raylib_math = raylib_dep.module("raylib-math"); // raymath module
+    const rlgl = raylib_dep.module("rlgl"); // rlgl module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
+    const cpDep = b.dependency("chipmunk", .{});
+
+    const cpLib = b.addStaticLibrary(.{
+        .name = "chipmunk",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const c_flags_default = &.{
+        "-std=gnu99",
+        "-ffast-math",
+        "-Wall",
+    };
+
+    // tell chipmunk to use floats, so we're not constantly swapping between
+    // f32 and f64 (raylib only supports f32)
+    // cpLib.root_module.addCMacro("CP_USE_DOUBLES", "0");
+
+    const c_flags_nondebug = &.{
+        "-DNDEBUG",
+    };
+
+    const c_flags: []const []const u8 = if (optimize != .Debug)
+        (std.mem.concat(b.allocator, []const u8, &.{ c_flags_default, c_flags_nondebug }) catch @panic("OOM"))
+    else
+        c_flags_default;
+
+    cpLib.addCSourceFiles(.{
+        .root = cpDep.path("."),
+        .files = &.{
+            "src/chipmunk.c",
+            "src/cpArbiter.c",
+            "src/cpArray.c",
+            "src/cpBBTree.c",
+            "src/cpBody.c",
+            "src/cpCollision.c",
+            "src/cpConstraint.c",
+            "src/cpDampedRotarySpring.c",
+            "src/cpDampedSpring.c",
+            "src/cpGearJoint.c",
+            "src/cpGrooveJoint.c",
+            "src/cpHashSet.c",
+            "src/cpHastySpace.c",
+            "src/cpMarch.c",
+            "src/cpPinJoint.c",
+            "src/cpPivotJoint.c",
+            "src/cpPolyShape.c",
+            "src/cpPolyline.c",
+            "src/cpRatchetJoint.c",
+            "src/cpRobust.c",
+            "src/cpRotaryLimitJoint.c",
+            "src/cpShape.c",
+            "src/cpSimpleMotor.c",
+            "src/cpSlideJoint.c",
+            "src/cpSpace.c",
+            "src/cpSpaceComponent.c",
+            "src/cpSpaceDebug.c",
+            "src/cpSpaceHash.c",
+            "src/cpSpaceQuery.c",
+            "src/cpSpaceStep.c",
+            "src/cpSpatialIndex.c",
+            "src/cpSweep1D.c",
+        },
+        .flags = c_flags,
+    });
+
+    cpLib.addIncludePath(cpDep.path("include"));
+    cpLib.installHeadersDirectory(cpDep.path("include/chipmunk"), "chipmunk", .{});
+    cpLib.linkLibC();
+
+    b.installArtifact(cpLib);
+
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibrary(cpLib);
+    // when zig is generating bindings for chipmunk, it needs to have this
+    // define to use floats (disable use of doubles)
+    // exe.root_module.addCMacro("CP_USE_DOUBLES", "0");
+    exe.linkLibrary(raylib_artifact);
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raylib-math", raylib_math);
+    exe.root_module.addImport("rlgl", rlgl);
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    // const lib_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    //
+    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    //
+    // const exe_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    //
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_lib_unit_tests.step);
+    // test_step.dependOn(&run_exe_unit_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,219 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+const cp = @cImport({
+    @cInclude("chipmunk/chipmunk.h");
+});
+
+pub fn main() !void {
+    //raylib init
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "slayloot");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    // init physics
+    const space = cp.cpSpaceNew() orelse return error.GenericError;
+    defer cp.cpSpaceFree(space);
+    const gravity = cp.cpv(0, 0);
+    cp.cpSpaceSetGravity(space, gravity);
+
+    // init game objects
+    var p = try Player.init(space);
+    defer p.deinit();
+
+    var walls: [42]Wall = undefined;
+    Wall.generateWalls(&walls, 16, 8);
+
+    // game loop
+    while (!rl.windowShouldClose() and rl.isKeyUp(rl.KeyboardKey.key_q)) {
+        // physics tick here
+        cp.cpSpaceStep(space, rl.getFrameTime());
+
+        // update game objects
+        p.update();
+
+        // draw game objects
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        p.draw();
+
+        for (walls) |wall| {
+            wall.draw();
+        }
+
+        rl.clearBackground(rl.Color.white);
+        rl.drawText("dungeon time", 190, 200, 20, rl.Color.light_gray);
+    }
+}
+
+fn updateAnyType(gameObj: anytype) void {
+    comptime switch (@typeInfo(@TypeOf(gameObj))) {
+        .Pointer => {},
+        else => @compileError("Why u no pass pointer?"),
+    };
+    gameObj.update();
+}
+
+fn normalize2d(vec: rl.Vector2) rl.Vector2 {
+    var out = vec;
+    var mag: f32 = std.math.sqrt(vec.x * vec.x + vec.y * vec.y);
+    mag = if (mag == 0) 1.0 else mag;
+
+    out.x /= mag;
+    out.y /= mag;
+
+    return out;
+}
+
+// const ColliderBoxComponent = struct {
+//     fn init() !void {}
+//     fn update() !void {}
+//     fn deinit() !void {}
+// };
+
+const DrawRectangleComponent = struct {
+    size: rl.Vector2,
+    color: rl.Color,
+
+    fn draw(self: DrawRectangleComponent, centerPos: rl.Vector2) void {
+        const offsetPos = .{ .x = centerPos.x - self.size.x / 2, .y = centerPos.y - self.size.y / 2 };
+        rl.drawRectangleV(offsetPos, self.size, self.color);
+    }
+};
+
+const Player = struct {
+    centerPos: rl.Vector2,
+    speed: f64,
+    rect: DrawRectangleComponent,
+    cpBody: *cp.cpBody,
+    cpShape: *cp.cpShape,
+
+    const mass = 80;
+    const width = 25;
+    const height = 25;
+    const radius = 30;
+
+    fn init(space: *cp.struct_cpSpace) !Player {
+        const rect = .{ .size = .{ .x = 25, .y = 25 }, .color = rl.Color.dark_green };
+        const pos = .{ .x = 50, .y = 50 };
+        const speed = 250;
+
+        const moment = cp.cpMomentForBox(mass, width, height);
+        const body = cp.cpBodyNew(mass, moment) orelse return error.GenericError;
+
+        //cpSpaceAddBody returns the same pointer we pass in... idk why
+        _ = cp.cpSpaceAddBody(space, body) orelse return error.GenericError;
+
+        cp.cpBodySetPosition(body, cp.cpv(pos.x, pos.y));
+
+        const shape = cp.cpBoxShapeNew(body, width, height, radius) orelse return error.GenericError;
+        //cpSpaceAddShape also returns the same pointer we pass in...
+        _ = cp.cpSpaceAddShape(space, shape) orelse return error.GenericError;
+
+        return Player{ .rect = rect, .centerPos = pos, .speed = speed, .cpBody = body, .cpShape = shape };
+    }
+
+    fn update(self: *Player) void {
+        const newPos = cp.cpBodyGetPosition(self.cpBody);
+        self.centerPos = .{ .x = @floatCast(newPos.x), .y = @floatCast(newPos.y) };
+
+        var moveVec = cp.cpVect {.x = 0, .y = 0};
+        if (rl.isKeyDown(rl.KeyboardKey.key_w)) {
+            moveVec.y -= 1;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_s)) {
+            moveVec.y += 1;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_d)) {
+            moveVec.x += 1;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_a)) {
+            moveVec.x -= 1;
+        }
+
+        moveVec = cp.cpvnormalize(moveVec);
+        moveVec = cp.cpvmult(moveVec, self.speed);
+
+        cp.cpBodySetVelocity(self.cpBody, moveVec);
+    }
+
+    fn draw(self: Player) void {
+        self.rect.draw(self.centerPos);
+    }
+
+    fn deinit(self: *Player) void {
+        // clean up cpShapes first then cpBody
+        cp.cpShapeFree(self.cpShape);
+        cp.cpBodyFree(self.cpBody);
+    }
+};
+
+const Wall = struct {
+    pos: rl.Vector2,
+    size: rl.Vector2,
+    color: rl.Color,
+
+    const colors = [_]rl.Color{
+        rl.Color.dark_gray,
+        rl.Color.light_gray,
+        rl.Color.gray,
+        rl.Color.orange,
+    };
+
+    fn draw(self: Wall) void {
+        rl.drawRectangleV(self.pos, self.size, self.color);
+    }
+
+    fn generateWalls(buffer: []Wall, width: comptime_int, height: comptime_int) void {
+        const side = enum { top, left, right, bottom };
+
+        var current_side = side.top;
+        var side_count: u32 = 0;
+        const side_size = height - 2; //- 2 for top and bottom walls
+
+        for (buffer, 0..) |*wall, i| {
+            switch (current_side) {
+                .top => {
+                    const x_pos: f32 = @floatFromInt(side_count * 25);
+                    wall.pos = .{ .x = x_pos, .y = 0 };
+                    if (side_count + 1 == width) {
+                        current_side = .bottom;
+                        side_count = 0;
+                    }
+                },
+                .bottom => {
+                    const x_pos: f32 = @floatFromInt(side_count * 25);
+                    const y_pos: f32 = @floatFromInt((height - 1) * 25);
+                    wall.pos = .{ .x = x_pos, .y = y_pos };
+                    if (side_count + 1 == width) {
+                        current_side = .left;
+                        side_count = 0;
+                    }
+                },
+                .left => {
+                    const y_pos: f32 = @floatFromInt(side_count * 25);
+                    const x_pos: f32 = 0;
+                    wall.pos = .{ .x = x_pos, .y = y_pos };
+                    if (side_count + 1 == side_size) {
+                        current_side = .right;
+                        side_count = 0;
+                    }
+                },
+                .right => {
+                    const y_pos: f32 = @floatFromInt(side_count * 25);
+                    const x_pos: f32 = @floatFromInt((width - 1) * 25);
+                    wall.pos = .{ .x = x_pos, .y = y_pos };
+                },
+            }
+
+            wall.size = .{ .x = 25, .y = 25 };
+            wall.color = colors[i % colors.len];
+            side_count += 1;
+        }
+    }
+};
